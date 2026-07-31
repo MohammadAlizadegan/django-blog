821287f422b22bcd218bf2070ea3c4329b044f4c
MohammadAlizadegan <alizadeganmo@gmail.com>
2026-07-31 12:51:14 +0330

    Add usermanager with BaseUserManager

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,8 +1,46 @@
 from django.db import models
 from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser, PermissionsMixin)
+from django.utils.translation import gettext_lazy as _
+
+
+class UserManager(BaseUserManager):
+    '''
+    custom user manager for the User model. This manager provides methods to create users and superusers.
+    '''
+    def create_user(self, email, password, **extra_fields):
+        '''
+        Creates and saves a User with the given email and password and extra fields.
+        '''
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        '''
+        Creates and saves a superuser with the given email and password and extra fields.
+        '''
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
+        
+
 
 class User(AbstractBaseUser, PermissionsMixin):
+    '''
+    Custom user model for the application. This model uses email as the unique identifier instead of username.
+    '''
     email = models.EmailField(unique=True, max_length=255)
+    is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     #is_verified = models.BooleanField(default=False)
@@ -14,6 +52,8 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email
