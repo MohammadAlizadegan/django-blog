5f368f7dcd66d8b10cb5440e04e9e99f957e0fe9
MohammadAlizadegan <alizadeganmo@gmail.com>
2026-08-03 13:37:50 +0330

    DetailView

---
+++ core/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = "blog"
+
+urlpatterns = [
+    path('cbv-index/', views.IndexView.as_view(), name='cbv-index'),
+    path('post/', views.PostList.as_view(), name = 'post-list'),
+    path('google/', views.RedirectToGoogle.as_view(), name='google'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail')
+]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView, RedirectView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Post
+from django.utils import timezone
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    """Shows index.html with CBV"""
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = 'AAA'
+        context['posts'] = Post.objects.all()
+        return context
+
+class RedirectToGoogle(RedirectView):
+    url = 'https://google.com'
+
+class PostList(ListView):
+    #model = Post
+    context_object_name = 'posts'
+    def get_queryset(self):
+        query = Post.objects.filter(status=True)
+        return query
+
+    paginate_by = 3
+
+class PostDetailView(DetailView):
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
