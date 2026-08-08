2037a61baed3bcd9775d05cfab4471c9712aa947
Mohammad Alizadegan <alizadeganmo@gmail.com>
2026-08-08 21:45:53 +0330

    کنترل نسخه Api در Restframework

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = "blog"
@@ -10,5 +10,6 @@ urlpatterns = [
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('create/', views.PostCreateView.as_view(), name='create'),
     path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-edit'),
-    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete')
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    path('api/v1/', include("blog.api.v1.urls")),
 ]
