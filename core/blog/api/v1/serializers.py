c985634e2d4ffd19f2270096977e1e79ae0249b9
Mohammad Alizadegan <alizadeganmo@gmail.com>
2026-08-09 00:55:12 +0330

    استفاده از ModelSerializer
    هیچگاه از "__all__" استفاده نمیگردد

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
+from ...models import Post
 
+# class PostSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
+#     status = serializers.BooleanField()
 
-class PostSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    status = serializers.BooleanField()
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Post
+        fields = ['id', 'title', 'content', 'created_at', 'published_at', 'status']
