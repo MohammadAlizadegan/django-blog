e914cb99bd68f41bb5cddb455e74c92cb237ccb2
Mohammad Alizadegan <alizadeganmo@gmail.com>
2026-08-08 22:41:09 +0330

    راه حل‌های بررسی وجود آیتم و Status Code‌های متناسب

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -2,6 +2,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import PostSerializer
 from ...models import Post
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 @api_view()
 def postList(request):
@@ -9,5 +11,10 @@ def postList(request):
 
 @api_view()
 def postDetail(request, id):
-    post = Post.objects.get(pk=id)
+    post = get_object_or_404(Post, pk=id)
     return Response(PostSerializer(post).data)
+    # try:
+    #     post = Post.objects.get(pk=id)
+    #     return Response(PostSerializer(post).data)
+    # except Post.DoesNotExist:
+    #     return Response({"detail":"object does not exist"}, status=status.HTTP_404_NOT_FOUND)
