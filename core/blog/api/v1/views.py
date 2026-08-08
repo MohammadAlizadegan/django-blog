8e8f62cf9d4ef740d1ee3db674c9cd25b9a66c91
Mohammad Alizadegan <alizadeganmo@gmail.com>
2026-08-08 22:28:04 +0330

    آشنایی با serializer و پاسخ‌دهی در درخواست برای یک پست

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from .serializers import PostSerializer
+from ...models import Post
 
 @api_view()
 def postList(request):
@@ -8,4 +9,5 @@ def postList(request):
 
 @api_view()
 def postDetail(request, id):
-    return Response(id)
+    post = Post.objects.get(pk=id)
+    return Response(PostSerializer(post).data)
