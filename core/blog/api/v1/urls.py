b9900f9cd5da0b1ff9dbdd98b9fd459b891ef40b
Mohammad Alizadegan <alizadeganmo@gmail.com>
2026-08-11 01:08:12 +0330

    آشنایی با DefaultRouter

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register('post', views.PostViewSet, basename='post')
+urlpatterns = router.urls
 
-urlpatterns = [
-    path('post/', views.PostViewSet.as_view({'get': 'list', 'post': 'create'}), name='post-list'),
-    path('post/<int:pk>/', views.PostViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='post-detail'),
-]
+# urlpatterns = [
+#     path('post/', views.PostViewSet.as_view({'get': 'list', 'post': 'create'}), name='post-list'),
+#     path('post/<int:pk>/', views.PostViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='post-detail'),
+#     #path('', include(router.urls)),
+# ]
+#urlpatterns += router.urls
