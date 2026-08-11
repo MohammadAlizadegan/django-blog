3adae28f9f31979865a23358efdde00956890b47
Mohammad Alizadegan <alizadeganmo@gmail.com>
2026-08-11 16:25:45 +0330

    Extrat actions for add other capibilities with auto urls

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,8 +1,10 @@
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+
 from .serializers import PostSerializer, CategorySerializer
 from ...models import Post, Category
 from rest_framework import viewsets, status
-
+from rest_framework.decorators import action
 
 class PostModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
@@ -11,6 +13,14 @@ class PostModelViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """This works better than queryset because queryset hs cache."""
         return Post.objects.filter(status=True)
+
+    @action(methods=['get'], detail=False)
+    def get_myposts(self, request):
+        myposts = Post.objects.filter(status=True).filter(author__user=self.request.user)
+        serializer = PostSerializer(myposts, many=True)
+        return Response(serializer.data)
+
+
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
