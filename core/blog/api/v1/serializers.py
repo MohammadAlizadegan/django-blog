dc77bd85e6a3d895cd197d99d7840f0004ff2ed3
Mohammad Alizadegan <alizadeganmo@gmail.com>
2026-08-12 00:17:03 +0330

    تشخیص درخواست لیست آیتم‌ها یا یک آیتم

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -9,16 +9,26 @@ from ...models import Post, Category
 #     title = serializers.CharField(max_length=255)
 #     status = serializers.BooleanField()
 
-class PostSerializer(serializers.ModelSerializer):
-    snippet = serializers.ReadOnlyField(source='get_snippet')
-    url = serializers.HyperlinkedIdentityField(view_name='blog:api-v1:post-detail', read_only=True)
-    category = serializers.SlugRelatedField(many=False, slug_field='name', queryset=Category.objects.all())
-    class Meta:
-        model=Post
-        fields = "__all__"
-
-
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
+        fields = ['id', 'name']
+
+
+
+class PostSerializer(serializers.ModelSerializer):
+    snippet = serializers.ReadOnlyField(source='get_snippet')
+    url = serializers.HyperlinkedIdentityField(view_name='blog:api-v1:post-detail', read_only=True)
+    class Meta:
+        model=Post
         fields = "__all__"
+    def to_representation(self, instance): #to change something in showing. (Show the name and create by id)
+        request = self.context.get('request')
+        rep = super().to_representation(instance) #get all parameters to show
+        #rep['category'] = CategorySerializer(instance.category).data #change 'category' parameter. instance is the post object.
+        if request.parser_context.get('kwargs').get('pk'): #Access to post-detail page.
+            rep.pop('snippet', None) #Delete 'snippet' from post-detail
+            rep.pop('url', None) #Delete 'url' from post-detail
+        else:
+            rep.pop('content', None) #Delete 'content' from post-list
+        return rep
