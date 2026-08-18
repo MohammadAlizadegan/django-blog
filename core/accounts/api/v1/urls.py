df5a127d0e4cc5b7e29a13b9ba442f2453016a6e
Mohammad Alizadegan <alizadeganmo@gmail.com>
2026-08-18 16:29:26 +0330

    پیاده‌سازی JWT با SimpleJWT

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,14 +1,23 @@
 from django.urls import path
 from . import views
 #from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from rest_framework_simplejwt.views import TokenVerifyView
 
 urlpatterns = [
     #registration
     path('registration/', views.RegistrationApiView.as_view(), name='registration'),
     path('token/login/', views.CustomObtainAuthToken.as_view(), name='token-login'),
     path('token/logout/', views.CustomDiscardAuthToken.as_view(), name='token-logout'),
+
     #ChangePassword
     #ResetPassword
     #LoginToken
     #Login JWT
+    path('jwt/create/', TokenObtainPairView.as_view(), name='jwt-create'),
+    path('jwt/refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
+    path('jwt/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
