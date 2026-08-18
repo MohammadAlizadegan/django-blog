35a24f95c9e9f0182104b257e5f0d5a564476d34
Mohammad Alizadegan <alizadeganmo@gmail.com>
2026-08-18 07:13:57 +0330

    شخصی‌سازی Token Authentication (response)

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+#from rest_framework.authtoken.views import ObtainAuthToken
 
 urlpatterns = [
     #registration
     path('registration/', views.RegistrationApiView.as_view(), name='registration'),
+    path('token/login/', views.CustomObtainAuthToken.as_view(), name='token-login'),
     #ChangePassword
     #ResetPassword
     #LoginToken
